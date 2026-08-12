243c67e341a00248853a431eb28d38cfc12bd4be
Aaron Farnham <aaron@tinyrobot.org>
2026-08-12 14:51:19 -0500

    fix: use current Codex granular approval policy

--- elixir/lib/symphony_elixir/config/schema.ex
+++ elixir/lib/symphony_elixir/config/schema.ex
@@ -350,7 +350,7 @@ defmodule SymphonyElixir.Config.Schema do
 
       field(:approval_policy, StringOrMap,
         default: %{
-          "reject" => %{
+          "granular" => %{
             "sandbox_approval" => true,
             "rules" => true,
             "mcp_elicitations" => true
